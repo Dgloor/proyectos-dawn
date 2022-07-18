dcd29f04692344a34bcc5c0fe89ea10e2dbc8e60
Danny Loor <danny_loor00@hotmail.com>
2022-07-17 20:47:18 -0500

    final

--- Proyecto2/js/app.js
+++ Proyecto2/js/app.js
@@ -4,6 +4,12 @@ async function getInfo(url) {
     return data["info"];
 }
 
+async function getTotalPersonajes(personajesURL) {
+    let response = await fetch(personajesURL);
+    let data = await response.json();
+    return data["info"]["count"];
+}
+
 async function setTotales(infoPersonajes, infoEpisodios, infoUbicaciones) {
     document.querySelector(".total-personajes").innerHTML =
         infoPersonajes["count"];
@@ -13,11 +19,11 @@ async function setTotales(infoPersonajes, infoEpisodios, infoUbicaciones) {
         infoUbicaciones["count"];
 }
 
-async function setUltimosEpisodios(url, temporada, infoEpisodios) {
+async function setUltimosEpisodios(episodiosURL, temporada, infoEpisodios) {
     let episodiosTemporada = [];
 
     for (let i = 1; i < infoEpisodios["pages"] + 1; i++) {
-        const response = await fetch(`${url}?page=${i}`);
+        const response = await fetch(`${episodiosURL}?page=${i}`);
         const data = await response.json();
         const episodios = data["results"];
 
@@ -33,6 +39,8 @@ async function setUltimosEpisodios(url, temporada, infoEpisodios) {
 
     episodiosTemporada = episodiosTemporada.slice(-5);
 
+    document.querySelector(".ultimos-episodios").innerHTML = "";
+
     for (let i = 0; i < episodiosTemporada.length; i++) {
         const episodio = episodiosTemporada[i];
 
@@ -47,13 +55,48 @@ async function setUltimosEpisodios(url, temporada, infoEpisodios) {
     }
 }
 
+async function setChartPersonajes(personajesURL) {
+    const personajesVivosURL = personajesURL + "/?status=alive";
+    const personajesMuertosURL = personajesURL + "/?status=dead";
+    const personajesDesconocidoURL = personajesURL + "/?status=unknown";
+
+    const totalPersonajesVivos = await getTotalPersonajes(personajesVivosURL);
+    const totalPersonajesMuertos = await getTotalPersonajes(
+        personajesMuertosURL
+    );
+    const totalPersonajesDesconocido = await getTotalPersonajes(
+        personajesDesconocidoURL
+    );
+    
+    const data = {
+        series: [
+            totalPersonajesVivos,
+            totalPersonajesMuertos,
+            totalPersonajesDesconocido,
+        ],
+    };
+
+    const sum = function (a, b) {
+        return a + b;
+    };
+
+    const options = {
+        labelInterpolationFnc: function (value) {
+            return Math.round((value / data.series.reduce(sum)) * 100) + "%";
+        },
+        width: "450px",
+        height: "350px",
+    };
+
+    new Chartist.Pie(".ct-estado-personajes", data, options);
+}
+
 async function loadData() {
     const apiURL = "https://rickandmortyapi.com/api/";
     const personajesURL = apiURL + "character";
     const episodiosURL = apiURL + "episode";
     const ubicacionesURL = apiURL + "location";
-
-    const temporadaActual = 2;
+    const temporadaActual = 1;
 
     const infoPersonajes = await getInfo(personajesURL);
     const infoEpisodios = await getInfo(episodiosURL);
@@ -61,6 +104,18 @@ async function loadData() {
 
     setTotales(infoPersonajes, infoEpisodios, infoUbicaciones);
     setUltimosEpisodios(episodiosURL, temporadaActual, infoEpisodios);
+    setChartPersonajes(personajesURL);
+
+    // Events
+    // Temporadas
+    const selectTemporadas = document.querySelector(".select-temporadas");
+    selectTemporadas.addEventListener("change", () => {
+        const temporadas = document.querySelector(".select-temporadas").options;
+        const temporadaActual =
+            temporadas[temporadas.selectedIndex].textContent.slice(-1);
+
+        setUltimosEpisodios(episodiosURL, temporadaActual, infoEpisodios);
+    });
 }
 
 window.addEventListener("DOMContentLoaded", () => {
